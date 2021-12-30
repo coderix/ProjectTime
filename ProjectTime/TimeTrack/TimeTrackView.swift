73be4144f8ea166686c7929574b21c6b6e329b2f
Dirk Neumann <dneumann42@googlemail.com>
2021-12-30 14:29:47 +0100

    initialize client

--- ProjectTime/TimeTrack/TimeTrackView.swift
+++ ProjectTime/TimeTrack/TimeTrackView.swift
@@ -12,8 +12,42 @@ struct TimeTrackView: View {
     @Environment(\.managedObjectContext) private var viewContext
     static let tag: String? = "TimeTrack"
     
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Client.name, ascending: true)],
+        animation: .default)
+    private var clients: FetchedResults<Client>
+
+    @State private var selectedClient: Client = Client()
+    @State private var firstRun = true
+    
+    init() {
+    //    _selectedClient = State(wrappedValue: clients.first ?? Client())
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("Client:")
+                    Picker("client", selection: $selectedClient) {
+                        ForEach(clients) { (client: Client) in
+                            Text(client.clientName).tag(client.self)
+                        }
+                    }
+                }
+              //  Text($selectedClient.wrappedValue.clientName)
+            }
+            .navigationTitle("Track your time")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+
+                if firstRun {
+                    selectedClient = clients.first!
+                    firstRun.toggle()
+                }
+               
+
+            }
+        }
     }
 }
 
