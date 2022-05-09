5d0dcad5d563ffdf2b62e1d0580f71e3aaade776
Dirk Neumann <dneumann42@googlemail.com>
2022-05-09 14:21:27 +0200

    delete complete

--- ProjectTime/Client/ClientsView.swift
+++ ProjectTime/Client/ClientsView.swift
@@ -10,6 +10,7 @@ import CoreData
 
 struct ClientsView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    @EnvironmentObject var dataController: DataController
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Client.name, ascending: true)],
@@ -20,6 +21,7 @@ struct ClientsView: View {
     
     @State var showingAddClientView = false
     @State private var showingDeleteConfirmation = false
+    @State private var clientToDelete: Client?
 
     private func addClient() {
         withAnimation {
@@ -41,21 +43,11 @@ struct ClientsView: View {
         }
     }
 
+ 
     private func deleteClients(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { clients[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate.
-                // You should not use this function in a shipping application,
-                // although it may be useful during development.
-                let nsError = error as NSError
-
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        for index in offsets {
+            clientToDelete = clients[index]
+            showingDeleteConfirmation.toggle()
         }
     }
 
@@ -77,7 +69,7 @@ struct ClientsView: View {
                                 })
                         }
                         // too dangerous without an alert
-                        .onDelete(perform: {_ in showingDeleteConfirmation.toggle()})
+                        .onDelete(perform: deleteClients)
                     }
                     .listStyle(InsetGroupedListStyle())
                 }
@@ -114,7 +106,12 @@ struct ClientsView: View {
             })
             
             .alert("Delete the client(s)?", isPresented: $showingDeleteConfirmation) {
-                Button("Delete") {}
+                Button("Delete", role: .destructive) {
+                    dataController.delete(clientToDelete!)
+                    dataController.save()
+                    
+                }
+                Button("Cancel", role: .cancel) {}
             }
 
             SelectSomethingView()
