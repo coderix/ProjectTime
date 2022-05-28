7da2fec8b9008ed8df992cda086e931fc43cf252
Dirk Neumann <dneumann42@googlemail.com>
2022-05-28 15:03:17 +0200

    Update EditProjectView.swift

--- ProjectTime/Project/EditProjectView.swift
+++ ProjectTime/Project/EditProjectView.swift
@@ -14,21 +14,21 @@ struct EditProjectView: View {
     @EnvironmentObject var dataController: DataController
     
     @FetchRequest var projects: FetchedResults<Project>
-
+    
     let project: Project
-
+    
     @State private var title: String
     @State private var originalTitle: String
     @State private var details: String
     @State private var clientName: String
     @State private var rate: Decimal
- //   @State private var rate =
-
+    
     @State private var showingDeleteConfirm = false
     
     
-        @State private var isExporting: Bool = false
+    @State private var isExporting: Bool = false
     @State private var document: ExportDocument?
+    @State private var now = ""
     
     init(project: Project) {
         self.project = project
@@ -42,36 +42,36 @@ struct EditProjectView: View {
         projectFetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Project.title, ascending: true)]
         self._projects = FetchRequest(fetchRequest: projectFetchRequest)
         
-       
+        
     }
-
-     func export() {
-         var csvString = "\("Date"),\("Task"),\("From"),\("To"),\("Time")\n\n"
-         for hour in project.projectHours {
-             csvString.append("\(hour.formattedStartDay),\(hour.task!.taskTitle),\(hour.formattedStartTime),\(hour.formattedEndTime),\(hour.durationString)\n")
-         }
-         csvString.append("Total Time: \(project.projectDurationString), Invoice: \(project.projectSalaryString)")
-         //print (csvString)
-         self.document = ExportDocument(message: csvString)
+    
+    func export() {
+        var csvString = "\("Date"),\("Task"),\("From"),\("To"),\("Time")\n\n"
+        for hour in project.projectHours {
+            csvString.append("\(hour.formattedStartDay),\(hour.task!.taskTitle),\(hour.formattedStartTime),\(hour.formattedEndTime),\(hour.durationString)\n")
+        }
+        csvString.append("Total Time: \(project.projectDurationString), Invoice: \(project.projectSalaryString)")
+        //print (csvString)
+        self.document = ExportDocument(message: csvString)
         
         isExporting = true
         
     }
     func update() {
-     //   project.client?.objectWillChange.send()
+        //   project.client?.objectWillChange.send()
         project.title = title
         project.details = details
         project.rate = NSDecimalNumber(decimal: rate as Decimal)
         project.timestamp = Date()
         presentationMode.wrappedValue.dismiss()
-
+        
     }
-
+    
     func delete() {
         dataController.delete(project)
         presentationMode.wrappedValue.dismiss()
     }
-
+    
     func cancel() {
         presentationMode.wrappedValue.dismiss()
     }
@@ -90,36 +90,43 @@ struct EditProjectView: View {
     }
     
     var body: some View {
-
+        
         NavigationView {
             VStack {
-
+                
                 Form {
                     Section(header: Text("Title")) {
                         TextField("Project Title", text: $title)
                     }
-
+                    
                     Section(header: Text("Rate")) {
                         TextField("Project Rate", value: $rate, format: .number)
-                                       // .textFieldStyle(.roundedBorder)
-                                        .keyboardType(.decimalPad)
-                                       // .padding()
+                        // .textFieldStyle(.roundedBorder)
+                            .keyboardType(.decimalPad)
+                        // .padding()
                     }
-
+                    
                     Section(header: Text("Details")) {
                         TextEditor(text: $details)
                     }
                     
-                    Button(action: { export() }, label: {
-                        Text("Export")
-                    })
-
+                    Button(action: {
+                        
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateStyle = .medium
+                        dateFormatter.timeStyle = .none
+                        dateFormatter.dateFormat = "yyyy-MM-dd"
+                        now = dateFormatter.string(from: Date())
+                        export() }, label: {
+                            Text("Export")
+                        })
+                    
                     Button("Delete this Project") {
                         showingDeleteConfirm.toggle()
                     }
-
+                    
                 }
-                .fileExporter(isPresented: $isExporting, document: document, contentType: .commaSeparatedText, defaultFilename: "ProjectTimeExport-\(project.projectTitle).csv") { result in
+                .fileExporter(isPresented: $isExporting, document: document, contentType: .commaSeparatedText, defaultFilename: "ProjectTimeExport-\(project.projectTitle)-\(now).csv") { result in
                     if case .success = result {
                         // Handle success
                     } else {
@@ -127,7 +134,7 @@ struct EditProjectView: View {
                     }
                 }
             }
-
+            
             .navigationTitle(project.projectTitle + " - " + project.clientName)
             .navigationBarTitleDisplayMode(.inline)
             .onDisappear(perform: dataController.save)
@@ -148,25 +155,25 @@ struct EditProjectView: View {
                         update()
                     }
                     .disabled(titleNotValid)
-
+                    
                 }
             }
-       //     .onChange(of: title) { _ in update() }
-       //     .onChange(of: details) { _ in update() }
+            //     .onChange(of: title) { _ in update() }
+            //     .onChange(of: details) { _ in update() }
         }
-
+        
     }
 }
 
 struct EditProjectView_Previews: PreviewProvider {
     static var dataController = DataController.preview
-
+    
     static var previews: some View {
         let client = Client(context: dataController.container.viewContext)
         client.id = UUID()
         client.timestamp = Date()
         client.name = "Example Client"
-
+        
         let project = Project(context: dataController.container.viewContext)
         project.id = UUID()
         project.title = "Example Project"
@@ -174,12 +181,12 @@ struct EditProjectView_Previews: PreviewProvider {
         project.rate = 76.50
         project.timestamp = Date()
         project.client = client
-
+        
         return NavigationView {
             EditProjectView(project: project)
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
         }
-
+        
     }
 }
